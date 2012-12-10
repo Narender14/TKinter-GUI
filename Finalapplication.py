8e244112e910fd47ad5fd02ca7bc7cf042da2e8d
Narender <NARENDER1412@GMAIL.COM>
2012-12-09 22:06:54 -0500

    Successfully Commited

---
+++ Finalapplication.py
@@ -0,0 +1,317 @@
+import sys
+import tkinter as obj
+from getpass import getpass
+
+
+if __name__=='__main__':
+	Name_string = raw_input("Enter the name\n")
+	Pass_string = getpass('Enter your password: ')
+	choice = raw_input("Enter the toolkit\n")
+	
+	
+	name_field1 = ""
+	phoneno_field1 = ""
+	rbgender1 = ""
+	spin_day1 = 0
+	drop_month1 = ""
+	spin_year1 = 0
+	byname_field1 = ""
+	byphoneno_field1 = ""
+	send_name_field1 = ""
+	send_message_area1 = ""
+
+	def OnClickClear():
+		messages_area.Clear_area()
+
+
+	def OnClickSave():	
+		global Name_string
+		global name_field
+		global phoneno_field
+		global rb_gender	
+		global spin_day
+		global drop_month
+		global spin_year							
+		name_field1 = name_field.Get_text()
+		name_field.Clear_field()	
+		phoneno_field1 = phoneno_field.Get_text() 
+		phoneno_field.Clear_field()
+		rbgender1 = rbgender.Get_selected()
+		print rbgender1
+		spin_day1 = spin_day.Get_value()
+		print spin_day1
+		drop_month1 = drop_month.Get_value()
+		#drop_month.Set_value("January")
+		spin_year1 = spin_year.Get_value()
+		path = Name_string + ".txt"
+		Contact_file2 = open(path, 'a+')
+		Contact_file2.write("1" + "\n")
+		Contact_file2.write(name_field1 + "\n")
+		Contact_file2.write(phoneno_field1 + "\n")
+		Contact_file2.write(rbgender1 + "\n")
+		Contact_file2.write(str(spin_day1) + "/" + str(drop_month1).strip('[]') + "/" +  str(spin_year1) + "\n")
+		Contact_file2.write("1" + "\n")
+		Contact_file2.close()
+		
+
+
+	def OnClickSearch():
+		global Name_string
+		global byname_field
+		global byphone_field
+		global check_byname
+		global check_byphone
+		global results_area
+		flag1 = 0
+		flag2 = 0
+		if(check_byname.Is_checked()):
+			byname_field1 = byname_field.Get_text() 
+			flag1 = 1
+			
+		Search_path = Name_string + ".txt"
+		Search_file = open(Search_path, 'r')
+		#results_area.Clear_area()
+		results_area.Clear_area()
+		results_area.Append_text(byname_field1)
+		if(flag1 == 1):
+			tag1 = 0
+			tag2 = 0
+			tag3 = 0
+			tag4 = 0
+			for line in Search_file:
+				
+				if(tag2 == 1):
+					
+					if(line == "1"+"\n" or line == "1"):
+						tag1 = 0
+						tag2 = 0
+					else:
+						print line
+						results_area.Append_text(line)
+				elif(tag3 == 0 and (line == "1"+"\n" or line == "1")):
+					
+					tag1 = 1
+				elif(tag3 == 1 and (line == "1"+"\n" or line == "1")):
+					
+					tag3 =0
+				else:
+					if(tag1 == 1 and (line == byname_field1 + "\n" or line == byname_field1)):
+						print line
+						tag2 = 1
+					else:
+						tag1 = 0
+						tag2 = 0
+						tag3 = 1
+		
+
+
+	def OnClickSend():
+		global send_name_field
+		global send_message_area
+		send_name_field1 = send_name_field.Get_text()
+		send_name_field.Clear_field()
+		send_message_area1 = send_message_area.Get_text()
+		print send_message_area1,"meaasge"
+		send_message_area.Clear_area()
+		#write it in the messaged.txt file
+		Message_file2 = open('messages.txt', 'a+r')
+		Message_file2.write("1" + "\n")
+		Message_file2.write(send_name_field1 + "\n")
+		global Name_string
+		Message_file2.write(Name_string + "\n")
+		Message_file2.write(send_message_area1 + "\n")
+		Message_file2.write("1"+ "\n")
+		Message_file2.close()
+
+	
+	def Check1():
+		
+		i = 0
+		j = 0
+		tag = 0
+		res = 0
+		count = 0
+		global Name_string
+		global Pass_string
+		Pass_file = open('LogIn.txt', 'a+r')
+		#print Pass_file
+		#Pass_file.readlines()
+		for line in Pass_file:
+			print line
+			if(count%2 == 0 and line == Name_string + "\n" or line == Name_string and j == 0):
+				
+				j = 1
+				tag = 1
+			else:
+				if(j == 1):
+					j = 0
+					if(line == Pass_string + "\n" or line == Pass_string):
+						res = 1	
+						
+					else:
+						break
+					
+		if(res == 1):
+			#Pass_file.close() decide if we want to close it or not
+			Check2()
+		elif(res == 0 and tag == 0):
+			
+			Pass_file.write(Name_string + "\n")
+			Pass_file.write(Pass_string + "\n")
+			#Pass_file.close()
+			Check2()
+		elif(res == 0 and tag == 1):
+			print "Password Incorrect - Try Again"
+			sys.exit("Password Incorrect")
+	
+
+	def Check2():
+		#------------------------------------------------------------------------------------------------
+		global frame
+		#------------------------------------------------------------------------------------------------
+		#Naveen's code would be present here
+		#retrieve the messages based on name
+		Message_file = open('messages.txt', 'a+r')
+		tag1 = 0
+		tag2 = 0
+		tag3 = 0
+		global Name_string
+		global message_area
+	
+		print Name_string
+		for line in Message_file:
+			
+			if(tag2 == 1):
+				
+				if(line == "1"+"\n" or line == "1"):
+					tag1 = 0
+					tag2 = 0
+				else:
+					print line
+					messages_area.Append_text(line)
+			elif(tag3 == 0 and (line == "1"+"\n" or line == "1")):
+				
+				tag1 = 1
+			elif(tag3 == 1 and (line == "1"+"\n" or line == "1")):
+				
+				tag3 =0
+			else:
+				if(tag1 == 1 and (line == Name_string + "\n" or line == Name_string)):
+					print line
+					tag2 = 1
+				else:
+					tag1 = 0
+					tag2 = 0
+					tag3 = 1
+				#Append the text of the text area with the text present in messages file where the name agrees 
+		
+		window.Display()    #Display the window
+		Message_file.close()
+	
+	window = obj.Dashboard('Application', 1000, 670)	
+
+	a_contacts = obj.Static_text(25, 25, text = "Add Contacts ---> ")			# Creating static text1
+	window.Add(a_contacts)
+	name = obj.Static_text(20, 65, text = "Name")
+	window.Add(name)
+
+	phoneno = obj.Static_text(20, 105, text = "Phone No")
+	window.Add(phoneno)
+
+	gender = obj.Static_text( 25, 150, text = "Gender")
+	window.Add(gender)
+
+	dob = obj.Static_text(-10, 195, text = "Date of Birth")
+	window.Add(dob)
+
+	name_field = obj.Text_field( 130, 60, length = 270)
+	window.Add(name_field)
+	
+	phoneno_field = obj.Text_field(130, 100, length = 270)
+	window.Add(phoneno_field)
+
+	rbgender = obj.Radio_button("Male",130, 150)					# Creating a Radio Button Set1			
+	rbgender.Add_radio_button("Female", 210, 150)
+	window.Add(rbgender)
+
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']			 # List of entries in combo-box
+	drop_month = obj.Drop_list(110, 190, "January", months, 125, 25)	# Creating a Combo box
+	window.Add(drop_month)
+
+	spin_day = obj.Spin_list(235, 190, 50, 25, 1, 31)
+	window.Add(spin_day)
+
+	spin_year = obj.Spin_list(320, 190, 75, 25,1950,2012)
+	window.Add(spin_year)
+
+	save_btn = obj.Create_button(170, 250, "Save", 85, 25)						# Creating Submit Button
+	save_btn.Click_listener(OnClickSave)
+	window.Add(save_btn)								# Adding it to frame1
+
+	messages = obj.Static_text(25, 300, text = "Messages Recieved --->")
+	window.Add(messages)
+
+	messages_area = obj.Text_area(25, 330, 375, 250)
+	window.Add(messages_area)
+
+	clear_btn = obj.Create_button(170, 600, "Clear", 85, 25)						# Creating Submit Button
+	clear_btn.Click_listener(OnClickClear)
+	window.Add(clear_btn)								# Adding it to frame1
+
+
+	s_contacts = obj.Static_text(500, 25, text = "Search Contacts ---> ")			# Creating static text1
+	window.Add(s_contacts)
+
+	byname = obj.Static_text(480, 65, text = "By Name")
+	window.Add(byname)
+
+	#byphoneno = obj.Static_text(480, 105, text = "By Phone No")
+	#window.Add(byphoneno)
+	
+	select = obj.Static_text(890, 25, text = "Select")
+	window.Add(select)
+
+	byname_field = obj.Text_field( 605, 60)
+	window.Add(byname_field)
+	
+	#byphoneno_field = obj.Text_field(605, 100)
+	#window.Add(byphoneno_field)
+
+	check_byname = obj.Check_box(900, 60, "", 20, 20)		# Adding entries
+	window.Add(check_byname)								# Adding it to frame1		
+
+	#check_byphoneno = obj.Check_box(900, 100, "" , 20, 20)		# Adding entries
+	#window.Add(check_byphoneno)								# Adding it to frame1		
+
+	search_btn = obj.Create_button(690, 160, "Search", 85, 25)						# Creating Submit Button
+	search_btn.Click_listener(OnClickSearch)
+	window.Add(search_btn)								# Adding it to frame1
+
+	results = obj.Static_text(500, 200, text = "Results --->")
+	window.Add(results)
+
+	results_area = obj.Text_area(500, 230, 450, 125)
+	window.Add(results_area)
+
+	message = obj.Static_text(500, 370, text = "Send Message --->")
+	window.Add(message)
+
+	send_name = obj.Static_text(500, 410, text = "Name")
+	window.Add(send_name)
+
+	send_message = obj.Static_text(500, 450, text = "Message")
+	window.Add(send_message)
+
+	send_name_field = obj.Text_field( 605, 405, length = 270)
+	window.Add(send_name_field)
+
+	send_message_area = obj.Text_area( 605, 445, 270, 140)
+	window.Add(send_message_area)
+
+	send_btn = obj.Create_button(690, 600, "Send", 85, 25)						# Creating Submit Button
+	send_btn.Click_listener(OnClickSend)
+	window.Add(send_btn)								# Adding it to frame1
+	
+	Check1()	
+
+
---
+++ tkinter.py
@@ -0,0 +1,360 @@
+
+import sys
+import Tkinter as root
+
+
+class Dashboard():
+    def __init__(self,title,length,breadth):
+        self.window = root.Tk()
+        self.window.title(title)
+        
+        # get screen width and height
+        ws = self.window.winfo_screenwidth()
+        hs = self.window.winfo_screenheight()
+        # calculate position x, y
+        x = (ws/2) - (length/2)    
+        y = (hs/2) - (breadth/2)
+        self.window.geometry('%dx%d+%d+%d' % (length, breadth, x, y))
+        
+        # To show the main window with checkbox , buttons etc.
+    def Display(self, choice = True):
+        self.window.mainloop()
+        return
+
+#Adding Button , checkbox , textarea , radiobutton as a widget which depends on passing argument
+    def Add(self,widget):
+        name = widget.name
+        if(name == "button"):
+            fram = self.fram(widget)  #Setting the widget into a fram
+            widget.controller = root.Button(fram, text=widget.title, command=widget.call)
+            widget.controller.pack(fill=root.BOTH, expand=1)
+
+        elif(name == "textarea"):
+            fram = self.fram(widget)  #Setting the widget into a fram
+            widget.controller = root.Text(fram)
+            widget.controller.pack(fill=root.BOTH, expand=1)
+
+        elif(name == "checkbox"):
+            fram = self.fram(widget)  # Setting the widget into a fram
+            var = widget.value         # Widget.value contain whether the widget need to be marked
+            widget.value = root.IntVar()
+            if(var):
+                widget.value.set(1)
+            else:
+                widget.value.set(0)
+            
+            widget.controller = root.Checkbutton(fram, text=widget.title, variable=widget.value, onvalue=1, offvalue=0)  # Creating the widget checkbutton
+            widget.controller.grid(sticky=root.W)
+            
+        elif(name == "radiobox"):
+            fram = self.fram1(widget.width,widget.height,widget.positions_X[0],widget.positions_Y[0])
+            widget.controller = [] #creating the list for widgets
+            radio_var = widget.value # Widget.value contain whether the widget need to be marked
+            widget.value = root.IntVar()
+            radio_controller = root.Radiobutton(fram, text=widget.labels[0], variable=widget.value, value=0)
+            radio_controller.pack(fill=root.BOTH, expand=1)
+            widget.controller.append(radio_controller)
+            for i in range(1,len(widget.labels)): #for each radiobutton , creating radiobutton acc, to pos.
+                fram = self.fram1(widget.width,widget.height,widget.positions_X[i],widget.positions_Y[i])
+                radio_controller = root.Radiobutton(fram, text=widget.labels[i], variable=widget.value,value=i)
+                radio_controller.pack(fill=root.BOTH, expand=1) #packing the radiobuttons
+                widget.controller.append(radio_controller)
+	elif(name=="textbox"):
+	    fram = self.fram(widget)
+	    widget.controller = root.Entry(fram)
+	    widget.controller.pack()
+	elif(name=="Password"):
+	    fram = self.fram(widget)
+	    widget.controller = root.Entry(fram,show="*")
+	    widget.controller.pack()
+	elif(name=="ValueList"):
+            array = ['']
+            array[0] = widget.value
+            array = array + widget.choices
+            widget.list_var = root.StringVar()
+            
+            widget.list_var.set(array[0])
+
+            fram = self.fram(widget)
+            widget.controller = apply(root.OptionMenu, (fram, widget.list_var) + tuple(array))
+            widget.controller.pack(fill=root.BOTH, expand=1)
+	elif(name=="spin"):
+	    fram = self.fram(widget)
+	    widget.controller=root.Spinbox(fram,from_=widget.min,to=widget.max)
+	    widget.controller.pack()
+
+
+	elif(name == "LabelText"):
+	   
+            temp = widget.label_var
+            widget.label_var = root.StringVar()
+            widget.label_var.set(temp)
+            fram = self.fram(widget)
+            widget.controller = root.Label(fram, textvariable=widget.label_var)
+            widget.controller.pack(fill=root.BOTH, expand=1)
+
+    def fram(self,widget): # Creating fram for widgets , thus setting width and height of widget
+	fram = root.Frame(self.window, width=widget.width,height=widget.height)
+        fram.pack_propagate(0) # don't shrink
+        fram.pack()
+        fram.place(x=widget.position_X,y=widget.position_Y) #positoin of widget within fram
+        return fram
+
+    def fram1(self,W,H,X,Y):   #Creating fram for radiobuttons
+        fram = root.Frame(self.window, width=W,height=H)
+        fram.pack_propagate(0) # don't shrink
+        fram.pack()
+        fram.place(x=X,y=Y)
+        return fram
+
+# FOR Button Widget ........................
+
+class Create_button:
+    controller = None
+    call = None
+    Type = None
+    def __init__(self,posX,posY,title="MyButton",length=100,breadth=30): #constructor setting pos_x,pos_y, width and height 
+        self.name = "button"
+        self.title = title
+        self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+
+    def Click_listener(self,method): #method for handling click Events
+        if(self.controller == None):
+            self.call = method
+        else:
+            self.controller.callback(method)
+        return True
+        
+# FOR TextArea Widget ........................
+
+class Text_area:
+    controller = None
+    def __init__(self,posX,posY,length=300,breadth=200,text=""): #constructor setting pos_x,pos_y , width and height
+        self.name = "textarea"
+        self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+
+    def Set_text(self,text): #Setting text for textarea
+        if(self.controller == None):
+            self.text = text
+        else:
+            self.controller.delete(1.0, root.END)
+            self.controller.insert(root.INSERT,text)
+        return True
+
+    def Get_text(self):
+        if(self.controller == None):
+            return self.text
+        else:
+            return self.controller.get(1.0, root.END)
+
+    def Append_text(self,text):
+	print "narender"	
+        if(self.controller == None):
+	    self.text = self.text + text
+        else:
+            self.controller.insert(root.INSERT, text)
+        return True                    
+
+    def Clear_area(self):
+        self.controller.delete(1.0, root.END)
+        return True
+#For TextBox Widget .........................
+
+class Text_field:
+    def __init__(self,posX,posY,text="",length=250,breadth=25):
+	self.name="textbox"
+	self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+
+    def Set_text(self,text): #Setting text for textarea
+        if(self.controller == None):
+            self.text = text
+        else:
+            self.controller.delete(1.0, root.END)
+            self.controller.insert(root.INSERT,text)
+        return True
+
+    def Get_text(self):          #Returning text which textarea is holding
+        if(self.controller == None):
+            return self.text
+        else:
+            return self.controller.get()
+
+    def Clear_field(self):
+        self.controller.delete(0,root.END)
+        return True	
+
+#######################################################################
+
+class Password_field:
+    def __init__(self,posX,posY,text="",length=250,breadth=25):
+	self.name="Password"
+	self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+
+    def Set_text(self,text): #Setting text for textarea
+        if(self.controller == None):
+            self.text = text
+        else:
+            self.controller.delete(1.0, root.END)
+            self.controller.insert(root.INSERT,text)
+        return True
+
+    def Get_text(self):          #Returning text which textarea is holding
+        if(self.controller == None):
+            return self.text
+        else:
+            return self.controller.get()
+
+    def Clear_field(self):
+        self.controller.delete(0,root.END)
+        return True	
+
+#######################################################################
+
+class Static_text:
+    controller = None
+    label_var = ""
+    def __init__(self,posX,posY,length=150,breadth=30,text=""):
+        
+	self.label_var=text
+        self.name= "LabelText"
+        self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+
+    def Set_value(self,text):
+        if(self.controller == None):
+            self.text = text
+        else:
+            self.label_var.set(text)
+        return True
+
+
+########################################################################
+
+
+
+# FOR CheckBox Widget ........................
+
+class Check_box:
+    controller = None
+    value = True
+
+    def __init__(self,posX,posY,title,length,breadth): #constructor setting pos_x,pos_y , width and height
+        self.name = "checkbox"
+        self.title=title
+        self.position_X = posX
+        self.position_Y = posY
+	self.width = length
+        self.height = breadth
+       
+    def Set_checked(self,value=True): #setting marked or unmarked for checkbox
+        if(self.controller == None):
+            self.value = value
+        else:
+	    if(value):
+                self.controller.select()
+            else:
+                self.controller.deselect()
+
+    def Is_checked(self): #Returning the value of checkbox(is it marked or unmarked)
+        if(self.controller == None):
+            return self.value
+        else:
+            if(self.value.get() == 1):  
+                return True
+            else:
+                return False
+		
+
+ 
+# FOR RadioButton Widget ........................
+ 
+class Radio_button:
+    controller = None
+    selected_index = None
+    Type = None
+    value = 0
+   
+    def __init__(self,label1,posX,posY):#Returning the value of checkbox(is it marked or unmarked)
+	self.positions_X = [] #creating empty list for positions for  radiobuttons 
+    	self.positions_Y = []
+	self.labels = []
+        self.name = "radiobox"
+	self.width=100
+	self.height=50
+	self.Add_radio_button(label1,posX,posY)       
+                                                            
+    def Add_radio_button(self,label,posX,posY):  #Adding the label and position for a particular radiobutton
+        self.labels.append(label)
+        self.positions_X.append(posX) #Appending the position in the position LIST
+        self.positions_Y.append(posY)
+        return True
+
+    def Get_selected(self):  #Returning the value for radiobutton(marked or unmarked)
+        for i in range(len(self.controller)):
+            if(self.value.get()==i):
+                return self.labels[i]
+        return None
+
+
+    #def setButtonTrue(self,index): #Setting the value for radiobutton(marked or unmarked)
+     #   if(self.controller == None):
+      #      self.value=index
+       # else:
+        #    button_controller = self.controller[index]
+         #   button_controller.select()
+##############################################################################
+
+class Drop_list:
+    controller = None
+    Type = None
+    list_var = 0
+    name=None
+    def __init__(self,posX,posY,name1,values,length=100,breadth=27):
+        self.name = "ValueList"
+        self.choices = values
+        self.position_X = posX
+        self.position_Y = posY
+        self.width = length
+        self.height = breadth
+        self.value = name1
+
+    def Get_value(self):
+            if(self.controller == None):
+                return self.title
+            else:
+                return self.list_var.get()
+
+
+#Spin Here and There
+
+class Spin_list:
+    def __init__(self,posX,posY,length=100,breadth=25,minimum=0,maximum=100):
+	self.name="spin"
+	self.position_X = posX
+        self.position_Y = posY
+	self.width=length
+	self.height=breadth
+	self.min = minimum
+        self.max = maximum
+
+    def Get_value(self):	
+	return 6
+
+
+#########################################################################
+#Here Comes Main Part , DEMO Part........................................
+
